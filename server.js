5bd1280e45ab57fef819bb25d414f69abbef0f37
ngekakis <nikogekakis@dhcp-10-5-21-115.wireless.rochester.edu>
2015-11-14 15:00:11 -0500

    Changed port to 1420

--- server.js
+++ server.js
@@ -55,8 +55,8 @@ app.post('/users', function (req, res) {
   res.send('OK');
 });
 
-// start the server on http://localhost:420/
-var server = app.listen(420, function () {
+// start the server on http://localhost:1420/
+var server = app.listen(1420, function () {
   var port = server.address().port;
   console.log('Server started at http://localhost:%s/', port);
 });
